dbec1fc7c90a37d05983a6934cb4ad7c2aba3bbb
Wilhelm Fors <askims_ik94w@hotmail.com>
2017-02-16 10:12:54 +0100

    Initial Commit

---
+++ Labb1Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Labb1Swift
+//
+//  Created by Wilhelm Fors on 16/02/17.
+//  Copyright © 2017 Wilhelm Fors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
